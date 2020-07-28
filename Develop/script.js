81621c6970c98ed3866a59b07e0247b13cd9a179
sotrnguy92 <sonnguyen092@gmail.com>
2020-07-27 18:55:57 -0700

    started the generatepassword function and added a way to check for password length

--- Develop/script.js
+++ Develop/script.js
@@ -7,8 +7,17 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
+function generatePassword() {
+  passwordLength = 0
+  while (passwordLength < 8 || passwordLength > 128){
+    passwordLength = prompt("How long would you like your password to be?");
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("Please choose a length between 8 and 128");
+    }
+  return("hello");
+  }
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
