4f12cf198eebc21938770ebc4ca74ddf5894ba43
sotrnguy92 <sonnguyen092@gmail.com>
2020-07-28 16:32:41 -0700

    got generate password func to concatenate lowercase letters

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let lowerString = 'abcdefghijklmnopqrstuvwxyz'
-let upperString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-let specialString = '!@#$%^&*'
-let numString = '0123456789'
+let lowerString = "abcdefghijklmnopqrstuvwxyz";
+let upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let specialString = "!@#$%^&*";
+let numString = "0123456789";
 
 // Write password to the #password input
 function writePassword() {
@@ -21,29 +21,36 @@ function generatePassword() {
   let specialCase;
   let showPassword = "";
 
-  while (passwordLength < 8 || passwordLength > 128){
-    passwordLength = prompt("How long would you like your password to be? Please choose between 8 and 128.");
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt(
+      "How long would you like your password to be? Please choose between 8 and 128."
+    );
     if (passwordLength < 8 || passwordLength > 128) {
       alert("Please choose a length between 8 and 128");
     }
   }
-  
-  lowerCase = confirm("Would you like your password to include lower case letters?");
 
-  upperCase = confirm("Would you like your password to include Uppercase letters?")
-  
-  numCase = confirm("Would you like to include a number in your password?")
+  lowerCase = confirm(
+    "Would you like your password to include lower case letters?"
+  );
 
-  specialCase = confirm("Would you like to include a special character?")
+  upperCase = confirm(
+    "Would you like your password to include Uppercase letters?"
+  );
 
-  while (showPassword.length < passwordLength){
-    if (lowerCase === true){
-      showPassword.concat()
+  numCase = confirm("Would you like to include a number in your password?");
+
+  specialCase = confirm("Would you like to include a special character?");
+
+  while (showPassword.length < Number(passwordLength)) {
+    if (lowerCase === true) {
+      showPassword = showPassword.concat(lowerString[Math.floor(Math.random() * 26)])
     }
   }
 
 
-
+  return showPassword;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
